b87ebad27ea3af76879016bcdaf46eb12317d6c5
Dondre Mclenan <d.mclenan@gmail.com>
2023-02-10 13:47:12 -0500

    updating my portfolio again 2

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -4,10 +4,12 @@ export default function topbar() {
     return (
         <div className="topbar">
             <div className="wrapper">
-                <div className="left"></div>
+                <div className="left">
                 <a href="#intro">asdsad</a>
-                <div className="right"></div>
             </div>
+            <h1>Hello</h1>
+            <div className="right"></div>
+        </div>
         </div>
     )
 }
