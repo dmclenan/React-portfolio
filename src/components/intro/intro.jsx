0b1171e831cd0c1c3862c1ee060a2dce29188b7f
dondre mclenan <dondremclenan@dondres-Mini.lan>
2023-02-25 12:49:54 -0500

    update menu bar8

--- src/components/intro/intro.jsx
+++ src/components/intro/intro.jsx
@@ -3,7 +3,19 @@ import "./intro.scss"
 export default function intro() {
   return (
     <div className="intro" id="intro">
-      
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/img/man.png" alt="" />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi There, I'm</h2>
+          <h1>Dondre Mclenan</h1>
+          <h3>Junior Web Developer</h3>
+
+        </div>
+      </div>
     </div>
   )
 }
