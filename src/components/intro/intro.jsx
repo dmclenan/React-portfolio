3c9a8871b1554fb917520e354ddb940fa6649762
dondre mclenan <dondremclenan@dondres-Mini.lan>
2023-03-03 20:05:02 -0500

    update arrow key1

--- src/components/intro/intro.jsx
+++ src/components/intro/intro.jsx
@@ -1,7 +1,20 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import "./intro.scss"
-  
+import { init } from 'ityped'
+import { useEffect, useRef } from "react"
 
 export default function intro() {
+  const textRef = useRef();
+
+  useEffect(() => {
+    init(textRef.current, { 
+      showCursor: true, 
+      backDelay:1500,
+      backSpeed: 60,
+      strings: ["Developer", "Designer", "content Creator" ],
+    });
+  }, []);
+
   return (
     <div className="intro" id="intro">
       <div className="left">
@@ -13,10 +26,12 @@ export default function intro() {
         <div className="wrapper">
           <h2>Hi There, I'm</h2>
           <h1>Dondre Mclenan</h1>
-          <h3>Junior Web Developer<span></span></h3>
+          <h3>
+            Freelance <span ref={textRef}></span>
+          </h3>
         </div>
         <a href="#portfolio">
-             <img src="assets/down.png" alt=""/>
+          <img src="assets/down.png" alt="" />
         </a>
       </div>
     </div>
